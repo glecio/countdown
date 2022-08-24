9b78e4f8d7f83a9b03128aa0c1af9907bb5f0025
Glecio Santos <glecio.santos@gmail.com>
2022-08-24 20:51:26 -0300

    Months calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,36 @@
-const year = 2023
+const year = 2022
+/* 0=January 1=Febuary ... 11=december */
 const month = 7
-const day = 24
-const hour = 0
-const minute = 0
+const day = 31
+const hour = 20
+const minute = 52
 const second = 0
 const milissecond = 00
+
 
 function countDown() {
     const today = new Date()
     const targetDate = new Date(year, month, day, hour, minute, second, milissecond)
+    //const targetDate = new Date('25, Sep 2023')
     const totalSeconds = (targetDate - today) / 1000
-    const days = Math.floor(totalSeconds / 3600 / 24)
+
+    const months = Math.floor(totalSeconds / 3600 / 24 / 30);
+    const days = Math.floor(totalSeconds / 3600 / 24) % 30
     const hours = Math.floor(totalSeconds / 3600) % 24
     const minutes = Math.floor(totalSeconds / 60) % 60
     const seconds = Math.floor(totalSeconds) % 60
 
-
+    const monthsElement = document.getElementById('months')
     const daysElement = document.getElementById('days')
     const hoursElement = document.getElementById('hours')
     const minutesElement = document.getElementById('minutes')
     const secondsElement = document.getElementById('seconds')
+
+    monthsElement.innerHTML = months
     daysElement.innerHTML = days
     hoursElement.innerHTML = hours
     minutesElement.innerHTML = formatTime(minutes)
     secondsElement.innerHTML = formatTime(seconds)
-    console.log(targetDate)
 }
 
 function formatTime(time) {
